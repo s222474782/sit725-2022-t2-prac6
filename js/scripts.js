cede7e9197f8cb9cdc02cd4cfec326c016061c35
s222474782 <118607327+s222474782@users.noreply.github.com>
2022-12-07 20:55:26 +0300

    done updating

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,65 @@
-const cardList = [
-    {
-        title: "Kitten 2",
-        image: "images/livedemo.jpg",
-        link: "About Kitten 2",
-        desciption: "Demo desciption about kitten 2"
-    },
-    {
-        title: "Kitten 3",
-        image: "images/livedemo.jpg",
-        link: "About Kitten 3",
-        desciption: "Demo desciption about kitten 3"
-    }
-]
+// const cardList = [
+//     {
+//         title: "Kitten 2",
+//         image: "images/livedemo.jpg",
+//         link: "About Kitten 2",
+//         desciption: "Demo desciption about kitten 2"
+//     },
+//     {
+//         title: "Kitten 3",
+//         image: "images/livedemo.jpg",
+//         link: "About Kitten 3",
+//         desciption: "Demo desciption about kitten 3"
+//     }
+// ]
+const getProjects = () => {
+    $.get('/api/projects',(response) => {
+        if(response.statusCode==200){
+            addCards(response.data);
+        }
+    })
+}
+
+
+
+
+// const submitForm = () => {
+//     let formData = {};
+//     formData.first_name = $('#first_name').val();
+//     formData.last_name = $('#last_name').val();
+//     formData.password = $('#password').val();
+//     formData.email = $('#email').val();
+
+//     console.log("Form Data Submitted: ", formData);
+// }
+
 const clickMe = () => {
     alert("Thanks for clicking me. Hope you have a nice day!")
 }
 
 const submitForm = () => {
     let formData = {};
-    formData.first_name = $('#first_name').val();
-    formData.last_name = $('#last_name').val();
-    formData.password = $('#password').val();
-    formData.email = $('#email').val();
-
+    formData.title = $('#title').val();
+    formData.image = $('#image').val();
+    formData.link = $('#link').val();
+    formData.description = $('#description').val();
     console.log("Form Data Submitted: ", formData);
-}
+     addProjectToApp(formData);
+    
+    }
+//ajax function...
+    const addProjectToApp = (project) => {
+     $.ajax({
+     url: '/api/projects',
+     data: project,
+     type: 'POST',
+     success: (result) => {
+     alert(result.message);
+     location.reload(); // it automatically reloads the page 
+     }
+     })
+    }
+    
 
 const addCards = (items) => {
     items.forEach(item => {
@@ -47,6 +82,6 @@ $(document).ready(function(){
     $('#formSubmit').click(()=>{
         submitForm();
     })
-    addCards(cardList);
+    getProjects();
     $('.modal').modal();
   });
--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 var express = require("express")
 var app = express()
 var cors = require('cors')
+let projectCollection;
 
 var port = process.env.port || 3000;
 
 app.use(express.static(__dirname+'/public'));
 app.use(express.json());
 app.use(express.urlencoded({extends: false}));
-app.use(cors())
+app.use(cors());
 
-app.get('/api/projects',(req,res) => {
-    res.json({statusCode: 200, data: cardList, message:"Success"})
-})
-
+// mongo connection
 const MongoClient = require('mongoDb').MongoClient;
-const uri = process.env.MONGO_URI
+
+
+// database connection
+const uri = 'mongodb+srv://sit725-2022-t2-prac4:chep@cluster0.alzn8sh.mongodb.net/?retryWrites=true&w=majority'
 const client = new MongoClient(uri,{ useNewUrlParser: true })
+
+// insert
+const insertProjects = (project,callback) => {
+    projectCollection.insert(project,callback);
+}
+
+const getProjects = (callback) => {
+    projectCollection.find({}).toArray(callback);
+}
 
 const createColllection = (collectionName) => {
       client.connect((err,db) => {
@@ -33,13 +43,13 @@ const createColllection = (collectionName) => {
 const cardList = [
     {
         title: "Kitten 2",
-        image: "images/kitten-2.jpg",
+        image: "images/livedemo.jpg",
         link: "About Kitten 2",
         desciption: "Demo desciption about kitten 2"
     },
     {
         title: "Kitten 3",
-        image: "images/kitten-3.jpg",
+        image: "images/livedemo.jpg",
         link: "About Kitten 3",
         desciption: "Demo desciption about kitten 3"
     }
@@ -57,8 +67,42 @@ const cardList = [
 //     var result = addNumbers(number1, number2);
 //     res.json({statusCode: 200, data: result, message: 'Success'});
 // });
+
+app.get('/api/projects',(req,res) => {
+    getProjects((err,result) => {
+        if(err) {
+            res.json({statusCode: 400, message:err})
+        }
+        else {
+            res.json({statusCode: 200, message:"success", data: result})
+        }
+    })
+   // res.json({statusCode: 200, data: cardList, message:"Success"})
+})
+app.post('/api/projects',(req,res) => {
+    console.log("New Project added", req.body)
+    var newProject = req.body;
+    insertProjects(newProject,(err, result) =>{
+        if(err) {
+            res.json({statusCode: 400, message:err})
+        }
+         else {
+            res.json({statusCode: 200, message:"Project Successfully added", data: result}) 
+         }
+    })
+    getProjects((err,result) => {
+        if(err) {
+            res.json({statusCode: 400, message:err})
+        }
+        else {
+            res.json({statusCode: 200, message:"success", data: result})
+        }
+    })
+   // res.json({statusCode: 200, data: cardList, message:"Success"})
+})
 app.listen(port,()=>{
     console.log("App listening to:http://localhost:"+port)
+    createColllection("pets");
 })
 
 
