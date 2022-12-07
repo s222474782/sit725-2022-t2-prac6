c39f692dcee24db6ac8cd970e31c53e060426e30
s222474782 <118607327+s222474782@users.noreply.github.com>
2022-12-07 18:11:23 +0300

    update

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
 var express = require("express")
 var app = express()
+var cors = require('cors')
+
 var port = process.env.port || 3000;
 
 app.use(express.static(__dirname+'/public'));
 app.use(express.json());
 app.use(express.urlencoded({extends: false}));
+app.use(cors())
 
 app.get('/api/projects',(req,res) => {
     res.json({statusCode: 200, data: cardList, message:"Success"})
 })
 
+const MongoClient = require('mongoDb').MongoClient;
+const uri = process.env.MONGO_URI
+const client = new MongoClient(uri,{ useNewUrlParser: true })
+
+const createColllection = (collectionName) => {
+      client.connect((err,db) => {
+       projectCollection = client.db().collection(collectionName);
+       if(!err) {
+        console.log('MongoDB Connected')
+      }
+       else {
+     console.log("DB Error: ", err);
+     process.exit(1);
+    }
+})
+}
+    
 const cardList = [
     {
         title: "Kitten 2",
