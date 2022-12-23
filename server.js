1cfcc6fe5b4224a4d37d4ff50e8af26ac8d85e1d
s222474782 <118607327+s222474782@users.noreply.github.com>
2022-12-23 21:47:16 +0300

    update

--- server.js
+++ server.js
@@ -52,7 +52,16 @@ app.post('/api/projects',(req,res) => {
 })
     
 
-
+app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
+    var firstNumber = parseInt(req.params.firstNumber) 
+    var secondNumber = parseInt(req.params.secondNumber)
+    var result = firstNumber + secondNumber || null
+    if(result == null) {
+      res.json({result: result, statusCode: 400}).status(400)
+    }
+    else { res.json({result: result, statusCode: 200}).status(200) } 
+})
+  
 // get projects
  const getProjects = (callback) => {
      projectCollection.find({}).toArray(callback);
---
+++ test/test.js
@@ -0,0 +1,28 @@
+var express = require("express")
+var expect  = require("chai").expect;
+var request = require("request");
+
+
+describe("Add Two strings", function() {
+    var url = "http://localhost:3000/addTwoNumbers/a/b";
+    it("should not returns status 200", function(done) {
+        request(url, function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done()
+          });
+    });
+    it("returns statusCode key in body to check if api gives right result should be 400", function(done) {
+        request(url, function(error, response, body) {
+            body = JSON.parse(body)
+            expect(body.statusCode).to.equal(400);
+            done()
+          });
+    });
+    it("returns the result as null", function(done) {
+        request(url, function(error, response, body) {
+            body = JSON.parse(body)
+            expect(body.result).to.be.a('null');
+            done()
+          });
+    });
+  });
